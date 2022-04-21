a9aaa6d6eda0abd6e761ac635015f800073969a4
tkadziolka <tmax0135@gmail.com>
2022-04-21 19:44:37 +0200

    Created seeding migration

--- config/db.php
+++ config/db.php
@@ -1,14 +1,11 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+$config = parse_ini_file( $_SERVER['DOCUMENT_ROOT'] . 'credentials.ini', true);
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+return [
+    'class'    => 'yii\db\Connection',
+    'dsn'      => $config['db']['dsn'],
+    'username' => $config['db']['username'],
+    'password' => $config['db']['password'],
+    'charset'  => 'utf8',
 ];
--- migrations/m150101_185401_seed_skills_table_migration.php
+++ migrations/m150101_185401_seed_skills_table_migration.php
@@ -1,25 +1,28 @@
 <?php
-require_once 'vendor/autoload.php';
 
 use yii\db\Migration;
+
+require_once 'vendor/autoload.php';
 
 class m150101_185401_seed_skills_table_migration extends Migration
 {
 
     public function up()
     {
+        // TODO Connect with SkillsTable
+
         $faker = Faker\Factory::create();
 
         $this->createTable('skills', columns: [
             'id' => $this->primaryKey(),
             'name' => $this->string()->notNull(),
-            'proficiency' => $this->float()->nonNull()
+            'proficiency' => $this->float()->notNull()
         ]);
 
         $this->insert('skills', [
             'id' => $faker->uuid(),
             'name' => $faker->name(),
-            'prof'
+            'proficiency' => $faker->randomFloat(),
         ]);
     }
 
