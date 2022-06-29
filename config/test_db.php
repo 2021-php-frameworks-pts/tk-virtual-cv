272ba7e9fd0a061c05827ba49998263f6f3f2c07
Tomasz Kądziołka <tkadziolka@MacBook-Pro-Tomasz.local>
2022-06-29 19:35:52 +0200

    Created paths to credentials file

--- config/test_db.php
+++ config/test_db.php
@@ -1,8 +1,11 @@
 <?php
-$config = parse_ini_file( $_SERVER['DOCUMENT_ROOT'] . 'credentials.ini', true);
+$config = parse_ini_file( $_SERVER['DOCUMENT_ROOT'] . '/' . 'credentials.ini', true);
 
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
-$db['dsn'] = $config['db']['dsn'];
+$db['dsn'] = $config['db']['driver']
+. ':host=' . $config['db']['host']
+. ';port=' . $config['db']['port']
+. ';dbname=' . $config['db']['schema'];
 
 return $db;
